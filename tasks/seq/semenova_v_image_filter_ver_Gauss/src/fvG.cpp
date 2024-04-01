62c3d4078141b751e8e77d6bd40bb74b066cad62
Semenova Veronika <74305462+nik-se0@users.noreply.github.com>
2024-04-01 22:03:04 +0300

    Update2 fvG.cpp

--- tasks/seq/semenova_v_image_filter_ver_Gauss/src/fvG.cpp
+++ tasks/seq/semenova_v_image_filter_ver_Gauss/src/fvG.cpp
@@ -14,7 +14,7 @@ std::vector<std::vector<int>> convert12(int* array, size_t rows, size_t cols) {
 bool ImageFilterVerGauss::validation() {
   internal_order_test();
 
-return !taskData->inputs.empty() && !taskData->outputs.empty() && !taskData->inputs_count.empty() &&
+  return !taskData->inputs.empty() && !taskData->outputs.empty() && !taskData->inputs_count.empty() &&
          !taskData->outputs_count.empty() && taskData->inputs[0] != nullptr && taskData->outputs[0] != nullptr &&
          taskData->outputs_count[1] == taskData->inputs_count[1] && taskData->outputs_count[0] >= 3 &&
          +taskData->outputs_count[1] >= 3;  // the image size <>= size of filter core
