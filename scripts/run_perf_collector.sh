9acbae9e4a340583e77dcb98fe63c0c74ddf09aa
Semenova Veronika <74305462+nik-se0@users.noreply.github.com>
2024-04-05 01:56:49 +0300

    Update run_perf_collector.sh

--- scripts/run_perf_collector.sh
+++ scripts/run_perf_collector.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./build/bin/omp_perf_tests
+#./build/bin/omp_perf_tests
 ./build/bin/seq_perf_tests
-./build/bin/stl_perf_tests
-./build/bin/tbb_perf_tests
+#./build/bin/stl_perf_tests
+#./build/bin/tbb_perf_tests
