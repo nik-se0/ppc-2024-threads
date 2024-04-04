6da34c2809a1c0a655de3a40f6e871f40625620e
Semenova Veronika <74305462+nik-se0@users.noreply.github.com>
2024-04-05 02:13:26 +0300

    Update generate_perf_results.sh

--- scripts/generate_perf_results.sh
+++ scripts/generate_perf_results.sh
@@ -1,3 +1,3 @@
-mkdir scripts/perf_stat_dir
+#mkdir scripts/perf_stat_dir
 source scripts/run_perf_collector.sh &> scripts/perf_stat_dir/perf_log.txt
 python3 scripts/create_perf_table.py --input scripts/perf_stat_dir/perf_log.txt --output scripts/perf_stat_dir
