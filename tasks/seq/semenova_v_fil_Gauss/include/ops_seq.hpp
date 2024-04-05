648b317de54cb19f0c329b9f41726150ec9adfab
Semenova Veronika <74305462+nik-se0@users.noreply.github.com>
2024-04-05 11:00:52 +0300

    Update ops_seq.hpp

--- tasks/seq/semenova_v_fil_Gauss/include/ops_seq.hpp
+++ tasks/seq/semenova_v_fil_Gauss/include/ops_seq.hpp
@@ -10,6 +10,20 @@
 class ImageFilGauss : public ppc::core::Task {
  public:
   explicit ImageFilGauss(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  ~ImageFilGauss() {
+   if(image!=nullptr){
+    for(int i = 0; i < n; ++i) {
+      delete[] image[i];
+    }
+    delete[] image;
+   }
+   if(filteredImage!=nullptr){
+    for(int i = 0; i < n; ++i) {
+         delete[] filteredImage[i];
+    }
+    delete[] filteredImage;
+   }
+  }
   bool pre_processing() override;
   bool validation() override;
   bool run() override;
@@ -18,7 +32,7 @@ class ImageFilGauss : public ppc::core::Task {
  private:
      double kernel[3][3] = {
          {1.0 / 16, 2.0 / 16, 1.0 / 16}, {2.0 / 16, 4.0 / 16, 2.0 / 16}, {1.0 / 16, 2.0 / 16, 1.0 / 16} };
-     int** image;
-     int** filteredImage;
+     int** image=nullptr;
+     int** filteredImage=nullptr;
      int n, m;
 };
