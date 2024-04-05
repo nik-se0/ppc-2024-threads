2b9d3768e9233cf595c8513d8a6d63a9a66e1707
Semenova Veronika <74305462+nik-se0@users.noreply.github.com>
2024-04-05 09:53:15 +0300

    Update ops_seq.cpp

--- tasks/seq/semenova_v_fil_Gauss/src/ops_seq.cpp
+++ tasks/seq/semenova_v_fil_Gauss/src/ops_seq.cpp
@@ -28,9 +28,9 @@ bool ImageFilGauss::pre_processing() {
 
     for (int i = 0; i < n; ++i) {
       for (int j = 0; j < m; j++) {
-        image[i][j] = d[i * m + j];
+        image[i][j] = d[i * т + j];
         image[i][j] = std::max(0, std::min(255, image[i][j]));
-        filteredImage[i][j] = filteredImage[i][j];
+        filteredImage[i][j] = image[i][j];
       }
     }
   } catch (...) {
