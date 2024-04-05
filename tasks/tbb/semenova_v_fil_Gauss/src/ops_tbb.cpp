89b02802ccfeae17ae1be43e8e935bd8299b7aa6
Semenova Veronika <74305462+nik-se0@users.noreply.github.com>
2024-04-05 13:29:40 +0300

    Update ops_tbb.cpp

--- tasks/tbb/semenova_v_fil_Gauss/src/ops_tbb.cpp
+++ tasks/tbb/semenova_v_fil_Gauss/src/ops_tbb.cpp
@@ -1,9 +1,15 @@
 // Copyright 2024 Semenova Veronika
 #include "tbb/semenova_v_fil_Gauss/include/ops_tbb.hpp"
 
-#include <tbb/tbb.h>
-//#include <tbb/parallel_for.h>
-//#include <tbb/blocked_range2d.h>
+
+int Min(int b, int c) {
+    return (b < c) ? b : c;
+}
+
+int Max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
 
 bool ImageFilGauss::validation() {
   internal_order_test();
@@ -11,7 +17,7 @@ bool ImageFilGauss::validation() {
   return !taskData->inputs.empty() && !taskData->outputs.empty() && !taskData->inputs_count.empty() &&
          !taskData->outputs_count.empty() && taskData->inputs[0] != nullptr && taskData->outputs[0] != nullptr &&
          taskData->outputs_count[1] == taskData->inputs_count[1] && taskData->outputs_count[0] >= 3 &&
-         taskData->outputs_count[1] >= 3;  // the image size <>= size of filter core
+         +taskData->outputs_count[1] >= 3;  // the image size <>= size of filter core
 }
 
 bool ImageFilGauss::pre_processing() {
@@ -33,7 +39,7 @@ bool ImageFilGauss::pre_processing() {
     for (int i = 0; i < n; ++i) {
       for (int j = 0; j < m; j++) {
         image[i][j] = d[i * n + j];
-        //image[i][j] = std::max(0, std::min(255, image[i][j]));
+        image[i][j] = Max(0, Min(255, image[i][j]));
         filteredImage[i][j] = image[i][j];
       }
     }
@@ -47,28 +53,19 @@ bool ImageFilGauss::pre_processing() {
 bool ImageFilGauss::run() {
   internal_order_test();
   try {
-    /*tbb::parallel_for(
-      tbb::blocked_range2d<int>(1, n - 1, 1, m - 1),
-      & {
+    tbb::parallel_for(
+      tbb::blocked_range2d<int>(1, n - 1, 1, m - 1), [&](const tbb::blocked_range2d<int>& r) {
         for(int i = r.rows().begin(); i != r.rows().end(); ++i) {
           for(int j = r.cols().begin(); j != r.cols().end(); ++j) {
-            double sum = 0;
-            sum = image[i - 1][j - 1] * kernel[0][0] + image[i - 1][j] * kernel[0][1] + image[i - 1][j + 1] * kernel[0][2] +
-                  image[i][j - 1] * kernel[1][0] + image[i][j] * kernel[1][1] + image[i][j + 1] * kernel[1][2] +
-                  image[i + 1][j - 1] * kernel[2][0] + image[i + 1][j] * kernel[2][1] + image[i + 1][j + 1] * kernel[2][2];
+            double sum = image[i - 1][j - 1] * kernel[0][0] + image[i - 1][j] * kernel[0][1] + image[i - 1][j + 1] * kernel[0][2] +
+                         image[i][j - 1] * kernel[1][0] + image[i][j] * kernel[1][1] + image[i][j + 1] * kernel[1][2] +
+                         image[i + 1][j - 1] * kernel[2][0] + image[i + 1][j] * kernel[2][1] + image[i + 1][j + 1] * kernel[2][2];
             filteredImage[i][j] = (int)sum;
           }
         }
       }
-    );*/
-    tbb::parallel_for(1, n - 1, & {
-      for (int j = 1; j < m - 1; ++j) {
-        double sum = image[i - 1][j - 1] * kernel[0][0] + image[i - 1][j] * kernel[0][1] + image[i - 1][j + 1] * kernel[0][2] +
-                     image[i][j - 1] * kernel[1][0] + image[i][j] * kernel[1][1] + image[i][j + 1] * kernel[1][2] +
-                     image[i + 1][j - 1] * kernel[2][0] + image[i + 1][j] * kernel[2][1] + image[i + 1][j + 1] * kernel[2][2];
-        filteredImage[i][j] = (int)sum;
-      }
-    });
+    );
+
   } catch (...) {
     return false;
   }
@@ -80,7 +77,7 @@ bool ImageFilGauss::post_processing() {
   try {
     for (int i = 0; i < n; ++i) {
       for (int j = 0; j < m; ++j) {
-        //filteredImage[i][j] = std::max(0, std::min(255, filteredImage[i][j]));
+        filteredImage[i][j] = Max(0, Min(255, filteredImage[i][j]));
       }
     }
     int* d = reinterpret_cast<int*>(taskData->outputs[0]);
