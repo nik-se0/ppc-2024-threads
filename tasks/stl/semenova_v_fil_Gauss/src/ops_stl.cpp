2a1747bd658475775cc4b8370b0cc15c012c38f4
Semenova Veronika <74305462+nik-se0@users.noreply.github.com>
2024-04-06 11:39:13 +0300

    fix ops_stl.cpp

--- tasks/stl/semenova_v_fil_Gauss/src/ops_stl.cpp
+++ tasks/stl/semenova_v_fil_Gauss/src/ops_stl.cpp
@@ -1,6 +1,5 @@
 // Copyright 2024 Semenova Veronika
 #include "stl/semenova_v_fil_Gauss/include/ops_stl.hpp"
-
 #include <thread>
 
 bool ImageFilGauss::validation() {
@@ -9,7 +8,7 @@ bool ImageFilGauss::validation() {
   return !taskData->inputs.empty() && !taskData->outputs.empty() && !taskData->inputs_count.empty() &&
          !taskData->outputs_count.empty() && taskData->inputs[0] != nullptr && taskData->outputs[0] != nullptr &&
          taskData->outputs_count[1] == taskData->inputs_count[1] && taskData->outputs_count[0] >= 3 &&
-         taskData->outputs_count[1] >= 3;  // the image size <>= size of filter core
+         +taskData->outputs_count[1] >= 3;  // the image size <>= size of filter core
 }
 
 /*bool ImageFilGauss::pre_processing() {
@@ -75,8 +74,8 @@ bool ImageFilGauss::pre_processing() {
 
     for (int i = 0; i < n; ++i) {
       for (int j = 0; j < m; j++) {
-        *imageIndex(i, j) = std::max(0, std::min(255, *imageIndex(i, j)));
-        *filteredIndex(i, j) = *imageIndex(i, j);
+        *imageIndex(i,j) = std::max(0, std::min(255, *imageIndex(i,j)));
+        *filteredIndex(i,j) = *imageIndex(i,j);
       }
     }
   } catch (...) {
@@ -86,6 +85,7 @@ bool ImageFilGauss::pre_processing() {
   return true;
 }
 
+/*
 bool ImageFilGauss::run() {
   internal_order_test();
   try {
@@ -94,12 +94,10 @@ bool ImageFilGauss::run() {
       threads.push_back(std::thread([&, i] {
         for (int j = 1; j < m - 1; ++j) {
           double sum = 0;
-          sum = *imageIndex(i - 1, j - 1) * kernel[0][0] + *imageIndex(i - 1, j) * kernel[0][1] +
-                *imageIndex(i - 1, j + 1) * kernel[0][2] + *imageIndex(i, j - 1) * kernel[1][0] +
-                *imageIndex(i, j) * kernel[1][1] + *imageIndex(i, j + 1) * kernel[1][2] +
-                *imageIndex(i + 1, j - 1) * kernel[2][0] + *imageIndex(i + 1, j) * kernel[2][1] +
-                *imageIndex(i + 1, j + 1) * kernel[2][2];
-          *filteredIndex(i, j) = (int)sum;
+          sum = *imageIndex(i - 1, j - 1) * kernel[0][0] + *imageIndex(i - 1, j) * kernel[0][1] + *imageIndex(i - 1, j + 1) * kernel[0][2] +
+                *imageIndex(i, j - 1) * kernel[1][0] + *imageIndex(i, j) * kernel[1][1] + *imageIndex(i, j + 1) * kernel[1][2] +
+                *imageIndex(i + 1, j - 1) * kernel[2][0] + *imageIndex(i + 1, j) * kernel[2][1] + *imageIndex(i + 1, j + 1) * kernel[2][2];
+          *filteredIndex(i,j) = (int)sum;
         }
       }));
     }
@@ -111,17 +109,58 @@ bool ImageFilGauss::run() {
   }
   return true;
 }
+*/
+
+
+bool ImageFilGauss::run() {
+  internal_order_test();
+  try {
+    int numThreads = std::thread::hardware_concurrency();
+    std::thread* threads = new std::thread[numThreads];
+    int rowsPerThread = (n-2)/numThreads;
+
+
+    for (int i = 0; i < numThreads; ++i) {
+        int startRow = i * rowsPerThread+1;
+        int endRow = (i == numThreads - 1) ? n-1 : (i + 1) * rowsPerThread+1;
+        int a = startRow+endRow;
+        a++;
+        threads[i] = std::thread([this, startRow, endRow]() {
+             for (int i = startRow; i < endRow; ++i) {
+                 for (int j = 1; j < m - 1; ++j) {
+                    double sum = 0;
+                     sum = *imageIndex(i - 1, j - 1) * kernel[0][0] + *imageIndex(i - 1, j) * kernel[0][1] + *imageIndex(i - 1, j + 1) * kernel[0][2] +
+                      *imageIndex(i, j - 1) * kernel[1][0] + *imageIndex(i, j) * kernel[1][1] + *imageIndex(i, j + 1) * kernel[1][2] +
+                      *imageIndex(i + 1, j - 1) * kernel[2][0] + *imageIndex(i + 1, j) * kernel[2][1] + *imageIndex(i + 1, j + 1) * kernel[2][2];
+            *filteredIndex(i,j) = (int)sum;
+        }
+    }
+        });
+    }
+
+    for (int i = 0; i < numThreads; ++i) {
+        threads[i].join();
+    }
+
+    delete[] threads;
+  } catch (...) {
+    return false;
+  }
+  return true;
+}
 
 bool ImageFilGauss::post_processing() {
   internal_order_test();
   try {
     for (int i = 0; i < n; ++i) {
       for (int j = 0; j < m; ++j) {
-        *filteredIndex(i, j) = std::max(0, std::min(255, *filteredIndex(i, j)));
+        *filteredIndex(i,j) = std::max(0, std::min(255, *filteredIndex(i,j)));
       }
     }
-  } catch (...) {
+  }
+   catch (...) {
     return false;
   }
   return true;
 }
+
